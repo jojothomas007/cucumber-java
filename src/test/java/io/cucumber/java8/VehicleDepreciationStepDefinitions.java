538eab7286e6da1699e2362f4193a154be3b57f5
Jojo Thomas <jojothomas007@gmail.com>
2025-03-28 10:17:07 +0530

    step_definitions file added for feature vehicle_depreciation_idv.feature

---
+++ src/test/java/io/cucumber/java8/VehicleDepreciationStepDefinitions.java
@@ -0,0 +1,104 @@
+```java
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.And;
+
+public class VehicleDepreciationStepDefinitions {
+
+    @Given("a policyholder has a vehicle")
+    public void a_policyholder_has_a_vehicle() {
+        // Implement this method
+    }
+
+    @When("the vehicle age is calculated")
+    public void the_vehicle_age_is_calculated() {
+        // Implement this method
+    }
+
+    @Then("the depreciation rate should be determined based on the schedule")
+    public void the_depreciation_rate_should_be_determined_based_on_the_schedule(io.cucumber.datatable.DataTable dataTable) {
+        // Implement this method with step definition logic for scenario outline using DataTable
+    }
+
+    @Given("a vehicle that is older than 5 years")
+    public void a_vehicle_that_is_older_than_5_years() {
+        // Implement this method
+    }
+
+    @When("I determine the IDV")
+    public void i_determine_the_IDV() {
+        // Implement this method
+    }
+
+    @Then("the IDV should be decided by mutual agreement")
+    public void the_IDV_should_be_decided_by_mutual_agreement() {
+        // Implement this method
+    }
+
+    @Given("a vehicle that is older than 4 years but not exceeding 5 years")
+    public void a_vehicle_that_is_older_than_4_years_but_not_exceeding_5_years() {
+        // Implement this method
+    }
+
+    @When("I calculate the depreciation")
+    public void i_calculate_the_depreciation() {
+        // Implement this method
+    }
+
+    @Then("the depreciation rate should be 50%")
+    public void the_depreciation_rate_should_be_50_percent() {
+        // Implement this method
+    }
+
+    @Given("a vehicle that is older than 3 years but not exceeding 4 years")
+    public void a_vehicle_that_is_older_than_3_years_but_not_exceeding_4_years() {
+        // Implement this method
+    }
+
+    @Then("the depreciation rate should be 40%")
+    public void the_depreciation_rate_should_be_40_percent() {
+        // Implement this method
+    }
+
+    @Given("a vehicle that is older than 2 years but not exceeding 3 years")
+    public void a_vehicle_that_is_older_than_2_years_but_not_exceeding_3_years() {
+        // Implement this method
+    }
+
+    @Then("the depreciation rate should be 30%")
+    public void the_depreciation_rate_should_be_30_percent() {
+        // Implement this method
+    }
+
+    @Given("a vehicle that is older than 1 year but not exceeding 2 years")
+    public void a_vehicle_that_is_older_than_1_year_but_not_exceeding_2_years() {
+        // Implement this method
+    }
+
+    @Then("the depreciation rate should be 20%")
+    public void the_depreciation_rate_should_be_20_percent() {
+        // Implement this method
+    }
+
+    @Given("a vehicle that is older than 6 months but not exceeding 1 year")
+    public void a_vehicle_that_is_older_than_6_months_but_not_exceeding_1_year() {
+        // Implement this method
+    }
+
+    @Then("the depreciation rate should be 15%")
+    public void the_depreciation_rate_should_be_15_percent() {
+        // Implement this method
+    }
+
+    @Given("a vehicle that is not exceeding 6 months old")
+    public void a_vehicle_that_is_not_exceeding_6_months_old() {
+        // Implement this method
+    }
+
+    @Then("the depreciation rate should be 5%")
+    public void the_depreciation_rate_should_be_5_percent() {
+        // Implement this method
+    }
+}
+```
